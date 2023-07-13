2cb8f4a0b4c8af065aa40e4b2fc9f2acd7e18013
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-07-13 21:29:46 +0600

    showAll button added to show all data

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const loadPhones = async(searchText) =>{
+const loadPhones = async(searchText,dataLimit) =>{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
 // displayPhones(data.data[0].brand);
 
-const displayPhones = phones =>{
+const displayPhones = (phones, dataLimit) =>{
  
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
+  
+    //display no phones
+    const noPhone = document.getElementById('no-phone-message');
+     if(phones.length == 0){
+       noPhone.classList.remove('d-none');
+     }
+     else{
+        noPhone.classList.add('d-none');
+     }
+      //display 20 phones only
+      const showAll = document.getElementById('showAll');
+        if(dataLimit && phones.length > 6){
+            phones = phones.slice(0,6 ); 
+            showAll.classList.remove('d-none');
+        } else{
+            showAll.classList.add('d-none');
+        }
+        //display all phones 
     phones.forEach(phone =>{
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
@@ -25,15 +43,36 @@ const displayPhones = phones =>{
            
         `;
         phoneContainer.appendChild(phoneDiv);
-        console.log(phone);
        
-    })
+    });
+    //stop loader
+    toggleLoader(false);
+}
+
+const processSearch = (dataLimit) =>{
+     //start loader
+     toggleLoader(true);
+     const searchField = document.getElementById('search-field');
+     const searchText = searchField.value;
+     loadPhones(searchText,dataLimit);
 }
 
 document.getElementById('btn-search').addEventListener('click', function(){
-     const searchField = document.getElementById('search-field');
-     const searchText = searchField.value;
-     loadPhones(searchText);
+    processSearch(10);
 })
 
-loadPhones();
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    processSearch();
+})
+
+const toggleLoader = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');   
+    }
+    else{
+        loaderSection.classList.add('d-none');  
+    }
+}
+
+// loadPhones();
